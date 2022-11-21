7fd7ea1db73d4311f8c98da6ebd8b536f697dc9c
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-11-21 23:36:39 +0200

    new material added with more code oop and styling tomorrow will work on short test and continue from there

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,10 +3,49 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Pizza',
+      amount: 5,
+      date: new Date(2022,1,2),
+    },
+    {
+      id: 'e2',
+      title: 'Pasta',
+      amount: 1,
+      date: new Date(2022,1,3),
+    },
+    {
+      id: 'e3',
+      title: 'Burger',
+      amount: 1.5,
+      date: new Date(2022,1,4),
+    },
+    {
+      id: 'e4',
+      title: 'fish & chips',
+      amount: 3,
+      date: new Date(2022,1,5),
+    },
+  ]
+
   return (
     <div>
       <h2>Lets come some react.js stuff</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem title={expenses[0].title} 
+                   amount={expenses[0].amount} 
+                   date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} 
+                   amount={expenses[1].amount} 
+                   date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} 
+                   amount={expenses[2].amount} 
+                   date={expenses[2].date}></ExpenseItem>
+      <ExpenseItem title={expenses[3].title} 
+                   amount={expenses[3].amount} 
+                   date={expenses[3].date}></ExpenseItem>
     </div>
   );
 }
--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,12 +1,20 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+function ExpenseItem(props){
+// const expenseDate = new Date(2022,2,28);
+// const expenseTitle = 'Ian expense';
+// const expenseAmount = 300;
+// const month = props.date.toLocaleString('en-US',{month:'long'});
+// const day = props.date.toLocaleString('en-US',{day:'2-digit'});
+// const year = props.date.getFullYear();
 
-function ExpenseItem(){
     return (
         <div className="expense-item">
-            <div>Date</div>
+         <ExpenseDate date={props.date} />
             <div className="expense-item__description">
+                <h2>{props.title}</h2>
                 <h2>Title</h2>
-                <div className="expense-item__price" >Amount</div>
+                <div className="expense-item__price" >${props.amount}</div>
             </div>
         </div>
     );
